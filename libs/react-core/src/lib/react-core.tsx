7ccb908577e8ef2d562deb3df64e701f0834e867
Chetan Khandla <ckhandla94@gmail.com>
2024-10-25 10:51:51 +0530

    setup basic libs

---
+++ libs/react-core/src/lib/react-core.tsx
@@ -0,0 +1,11 @@
+import styles from './react-core.module.scss';
+
+export function ReactCore() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to ReactCore!</h1>
+    </div>
+  );
+}
+
+export default ReactCore;
