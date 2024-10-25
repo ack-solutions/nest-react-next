ff03bb8a9d33c152f625258ef27e5de0861ff34a
Chetan Khandla <ckhandla94@gmail.com>
2024-10-25 09:20:15 +0530

    update generator

--- tools/generators/jest.config.ts
+++ tools/generators/jest.config.ts
@@ -1,9 +1,9 @@
 export default {
-  displayName: 'crud',
+  displayName: 'generators',
   preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/tools/crud',
+  coverageDirectory: '../../coverage/tools/generators',
 };
