07fd6d89573e9a74a9c13c9a6ef3a549a7d0f9ef
kishan Khandala <kishan.ackplus@gmail.com>
2024-11-29 21:11:04 +0530

    update change password dialog

--- apps/admin/src/app/sections/user/change-password-dialog.tsx
+++ apps/admin/src/app/sections/user/change-password-dialog.tsx
@@ -1,115 +1,125 @@
 import { DefaultDialog, Icon, TextField } from '@admin/app/components'
-import { useBoolean, UserService, useToasty } from '@libs/react-core'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { FormContainer, RHFTextField, useBoolean, UserService, useToasty, useUserQuery } from '@libs/react-core'
 import { IUser } from '@libs/types'
 import { Button, IconButton, InputAdornment, Stack, useTheme } from '@mui/material'
-import { Field, Form, Formik, FormikHelpers } from 'formik'
 import { pick } from 'lodash'
 import { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
 import { object, ref, string } from 'yup'
-
 export interface ChangePasswordDialogProps {
     onClose?: () => void
     values?: IUser
 }
 
-const userService = UserService.getInstance<UserService>();
-
 const defaultValues = {
-
     password: '',
     confirmPassword: '',
 };
-const validationSchema = object().shape({
+const validationSchema = yupResolver(object().shape({
     password: string().label('New Password').required().matches(
         /^(?=.*[a-z])(?=.*[0-9])(?=.{8,})/,
         "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
     ),
     confirmPassword: string().label('Confirm Password').oneOf([ref('password'), ''], 'Passwords must match').required(),
-});
+}));
 
-const ChangePasswordDialog = ({ onClose, values }: ChangePasswordDialogProps) => {
+const ChangePasswordDialog = ({ onClose, values: initialValue }: ChangePasswordDialogProps) => {
     const showPassword = useBoolean()
     const confirmShowPassword = useBoolean()
     const { showToasty } = useToasty()
     const theme = useTheme()
+    const formContext = useForm({
+        defaultValues,
+        resolver: validationSchema,
+    })
+    const { reset } = formContext;
+    const { useUpdateUser } = useUserQuery()
+    const { mutate: updateUser } = useUpdateUser()
 
     const handleSubmit = useCallback(
-        (values: any, action: FormikHelpers<any>) => {
+        (values: any) => {
             const request = {
-                ...(pick(values, 'password'))
+                ...(pick(values, 'password')),
+                id: initialValue?.id,
             }
-            userService.update(values?.id, request).then(() => {
-                action.setSubmitting(false)
-                action.resetForm()
-                showToasty('Password Successfully updated')
-                onClose && onClose()
-            }).catch((error: any) => {
-                action.setSubmitting(false)
-                action.resetForm()
-                showToasty(error.message, 'error')
-            });
-        }, []
+            const options = {
+                onSuccess: (data) => {
+                    showToasty('Password Successfully updated')
+                    reset()
+                    onClose && onClose()
+                },
+                onError: (error) => {
+                    showToasty(error, 'error');
+                    reset()
+                    onClose && onClose()
+                }
+            }
+            updateUser(request, options);
+        }, [initialValue]
     )
+
     return (
         <DefaultDialog
             onClose={onClose}
             maxWidth='sm'
-            title='ChangePassword'
+            title='Reset Password'
         >
-            <Formik
-                initialValues={Object.assign({}, defaultValues, values)}
+            <FormContainer
+                FormProps={{
+                    id: "reset-password"
+                }}
+                formContext={formContext}
                 validationSchema={validationSchema}
-                onSubmit={handleSubmit}
+                onSuccess={handleSubmit}
             >
-                {({ handleSubmit }) => (
-                    <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
-                        <Stack spacing={2}>
-                            <Field
-                                fullWidth
-                                type={showPassword.value ? 'text' : 'password'}
-                                name="password"
-                                label="Password"
-                                component={TextField}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton onClick={() => showPassword.onToggle()} edge="end">
-                                                <Icon
-                                                    icon={showPassword.value ? 'eye' : 'eye-slash'}
-                                                    color={theme.palette.grey[500]}
-                                                />
-                                            </IconButton>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
+                <Stack spacing={2}>
+                    <RHFTextField
+                        fullWidth
+                        type={showPassword.value ? 'text' : 'password'}
+                        name="password"
+                        label="Password"
+                        slotProps={{
+                            input: {
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton onClick={() => showPassword.onToggle()} edge="end">
+                                            <Icon
+                                                icon={showPassword.value ? 'eye' : 'eye-slash'}
+                                                color={theme.palette.grey[500]}
+                                            />
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }
+                        }}
+                    />
 
-                            <Field
-                                fullWidth
-                                type={confirmShowPassword.value ? 'text' : 'password'}
-                                name="confirmPassword"
-                                label="Confirm Password"
-                                component={TextField}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton onClick={() => confirmShowPassword.onToggle()} edge="end">
-                                                <Icon
-                                                    icon={confirmShowPassword.value ? 'eye' : 'eye-slash'}
-                                                    color={theme.palette.grey[500]}
-                                                />
-                                            </IconButton>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                            <Button type='submit' variant='contained'>
-                                Change Password
-                            </Button>
-                        </Stack>
-                    </Form>
-                )}
-            </Formik>
+                    <RHFTextField
+                        fullWidth
+                        type={confirmShowPassword.value ? 'text' : 'password'}
+                        name="confirmPassword"
+                        label="Confirm Password"
+                        slotProps={{
+                            input: {
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton onClick={() => confirmShowPassword.onToggle()} edge="end">
+                                            <Icon
+                                                icon={confirmShowPassword.value ? 'eye' : 'eye-slash'}
+                                                color={theme.palette.grey[500]}
+                                            />
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }
+                        }}
+                    />
+                    <Button type='submit' variant='contained'>
+                        Change Password
+                    </Button>
+                </Stack>
+            </FormContainer>
         </DefaultDialog>
     )
 }
