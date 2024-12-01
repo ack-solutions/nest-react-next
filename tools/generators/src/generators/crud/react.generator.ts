199cc7aefc468786dd10a690d482f2043577b8b4
Chetan Khandla <ckhandla94@gmail.com>
2024-12-01 10:31:15 +0530

    fix: react crud generator

--- tools/generators/src/generators/crud/react.generator.ts
+++ tools/generators/src/generators/crud/react.generator.ts
@@ -2,6 +2,7 @@ import { generateFiles, getProjects, names, ProjectConfiguration, Tree } from "@
 import { PluginGeneratorSchema } from "./schema";
 import { join } from "path";
 import { toTitleCase } from "../../utils/to-title-case";
+import { addExportStatement } from "../../utils/add-export-statement";
 
 
 export class ReactGenerator {
@@ -40,26 +41,22 @@ export class ReactGenerator {
             `apps/admin/src/app`, // Destination where the custom files should go
             { tmpl: '', ...this.names, columns: this.options.columns } // Data to pass to the template (e.g., the library name)
         );
-    }
 
+        // Generate files in react core lib
+        generateFiles(
+            this.tree,
+            join(__dirname, 'files', 'react'), // Path to your custom template files
+            `libs/react-core/src/lib`, // Destination where the custom files should go
+            { tmpl: '', ...this.names, columns: this.options.columns } // Data to pass to the template (e.g., the library name)
+        );
+    }
 
     addExportStatement() {
-        const indexPath = `${this.projects.get('types').root}/src/index.ts`;
+        // Add Export in types
+        addExportStatement(this.tree, `${this.projects.get('types').root}/src/index.ts`, `export * from './lib/${this.names.fileName}';`)
 
-        // Check if index.ts exists
-        if (!this.tree.exists(indexPath)) {
-            throw new Error(`index.ts not found at ${indexPath}`);
-        }
-
-        // Read the current content of index.ts
-        const currentContent = this.tree.read(indexPath, 'utf-8').trim();
-
-        // Add the new export line (if it doesn't already exist)
-        const newExportLine = `export * from './lib/${this.names.fileName}';`;
-        if (!currentContent.includes(newExportLine)) {
-            const updatedContent = `${currentContent}\n${newExportLine}\n`;
-            this.tree.write(indexPath, updatedContent);
-        }
+        // Add Export in react core
+        addExportStatement(this.tree, `${this.projects.get('types').root}/src/lib/query-hooks/index.ts`, `export * from './use-${this.names.fileName}';`)
+        addExportStatement(this.tree, `${this.projects.get('types').root}/src/lib/services/index.ts`, `export * from './${this.names.fileName}.service';`)
     }
-
 }
---
+++ tools/generators/src/utils/add-export-statement.ts
@@ -0,0 +1,17 @@
+
+// Add the import statement if not already present
+export function addExportStatement(tree, filePath: string, newExportLine: string) {
+
+    // Check if index.ts exists
+    if (!tree.exists(filePath)) {
+        throw new Error(`File not found at ${filePath}`);
+    }
+
+    // Read the current content of index.ts
+    const currentContent = tree.read(filePath, 'utf-8').trim();
+
+    if (!currentContent.includes(newExportLine)) {
+        const updatedContent = `${currentContent}\n${newExportLine}\n`;
+        tree.write(filePath, updatedContent);
+    }
+}
