6c8ea767d71a359b7fc91a8dc27df569a47f1216
Chetan Khandla <ckhandla94@gmail.com>
2025-01-09 12:08:49 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ module.exports = [
             '.github',
             '.nx',
             '.vscode',
+            '**/.next/**',
 
             // Project Specific
             'apps/portal/src/assets',
