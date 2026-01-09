8d89a87a3cc68b52acb02a1a0eb1384f3c426105
Chetan Khandla <ckhandla94@gmail.com>
2026-01-09 15:33:34 +0530

    Setup crud generator UI (#31)
    * setup crud generator
    
    * cleanup-old code
    
    * celanup and fix issue
    
    * fix: tool generator
    
    * fix
    
    * fix: curd

--- eslint.config.js
+++ eslint.config.js
@@ -1,57 +1,29 @@
-const nx = require('@nx/eslint-plugin');
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
 const stylisticPlugin = require('@stylistic/eslint-plugin');
 const stylisticJsPlugin = require('@stylistic/eslint-plugin-js');
 const importPlugin = require('eslint-plugin-import');
 
+// Load custom base config
 const eslintConfig = require('./eslint-config/base-config');
 
-module.exports = [
-    {
-        files: ['*.json', '**/*.json'],
-        // Override or add rules here
-        rules: {'@typescript-eslint/no-unused-expressions': 'off'},
-        languageOptions: {parser: require('jsonc-eslint-parser')},
-    },
-    ...nx.configs['flat/base'],
-    ...nx.configs['flat/typescript'],
-    ...nx.configs['flat/javascript'],
+module.exports = tseslint.config(
     {
         ignores: [
             'node_modules/',
             'dist/',
             'tmp/',
             '.github',
-            '.nx',
             '.vscode',
             '**/.next/**',
             'apps/portal/src/assets',
             '**/vite.config.*.timestamp*',
             '**/vitest.config.*.timestamp*',
+            'tools/generators/**/templates/**',
         ],
     },
-    {
-        files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-        rules: {
-            '@nx/enforce-module-boundaries': [
-                'error',
-                {
-                    enforceBuildableLibDependency: true,
-                    allow: [
-                        '^@api/',
-                        '^@web/',
-                        '^@admin/',
-                        '^.*/eslint(\\.base)?\\.config\\.[cm]?js$',
-                    ],
-                    depConstraints: [
-                        {
-                            sourceTag: '*',
-                            onlyDependOnLibsWithTags: ['*'],
-                        },
-                    ],
-                },
-            ],
-        },
-    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
     {
         plugins: {
             import: importPlugin,
@@ -78,8 +50,7 @@ module.exports = [
         },
     },
     {
-        files: ['**/package.json', '**/generators.json'],
-        rules: {'@nx/nx-plugin-checks': 'error'},
-        languageOptions: {parser: require('jsonc-eslint-parser')},
-    },
-];
+        files: ['*.json', '**/*.json'],
+        rules: { '@typescript-eslint/no-unused-expressions': 'off' },
+    }
+);
